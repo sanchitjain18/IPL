1025275a22ee4a8dc56d1f5ef7d4052018b39d2d
Sanchit <sanchitjain.jain34@gmail.com>
2020-07-02 11:16:12 +0530

    first commit

---
+++ mysite/ipl/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import csv
+
+# Create your models here.
+
+#with open('matches.csv', 'r') as csv_file:
+ #   csv_reader = csv.reader(csv_file)
+  #  next(csv_reader)
+   # win = dict()
+    #mpy = dict()
+    #for line in csv_reader:
+     #   mpy[line[1]] = mpy.get(line[1], 0) + 1
+      #  win[line[10]] = win.get(line[10], 0) + 1
+
